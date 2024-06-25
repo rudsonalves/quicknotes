fb216e72ec8752fe17101630222583c4890f9bc3
Rudson R Alves <rudsonalves67@gmail.com>
2024-06-25 10:58:11 -0300

    done (#4)

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -1,236 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
-	"golang.org/x/crypto/bcrypt"
+	"github.com/rudsonalves/quicknotes/internal/mailer"
+	"gopkg.in/gomail.v2"
 )
 
-func delay(password string, function func(string) ([]byte, error)) ([]byte, error) {
-	begin := time.Now()
-	hash, err := function(password)
-	fmt.Println("Delay:", time.Since(begin))
-	return hash, err
+func main1() {
+	m := gomail.NewMessage()
+	m.SetHeader("From", "quicknotes@quick.com")
+	m.SetHeader("To", "alvestest67@gmail.com", "alvesdev67@gmail.com")
+	// m.SetAddressHeader("Cc", "dan@example.com", "Dan")
+	m.SetHeader("Subject", "Bem Vindo!")
+	m.SetBody("text/html", "<h1>Hello</h1> <p>Seja bem vindo ao <b>Quicknote</b>!</p>")
+	// m.Attach("/home/Alex/lolcat.jpg")
+
+	d := gomail.NewDialer("localhost", 1025, "", "")
+
+	// Send the email to Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
 }
 
-func BCRYPT(password string) ([]byte, error) {
-	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+func main2() {
+	// testar envio de email
+	msg := mailer.MailMessage{
+		To:      []string{"alvesdev67@gmail.com", "alvestest67@gmail.com"},
+		Subject: "Bem vindo2",
+		Body:    []byte("Seja bem vindo ao Quicknotes!"),
+		IsHtml:  false,
+	}
+
+	smtp := mailer.SMTPConfig{
+		Host:     "localhost",
+		Port:     1025,
+		UserName: "",
+		Password: "",
+		From:     "quicknote@quick.com",
+	}
+
+	mailservice := mailer.NewSmtpMailService(smtp)
+	mailservice.Send(msg)
 }
 
 func main() {
-	hash, err := delay("123456", BCRYPT)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(hash))
-
-	err = bcrypt.CompareHashAndPassword(hash, []byte("123456"))
-	if err != nil {
-		fmt.Println("Senha incorreta")
-	} else {
-		fmt.Println("Senha correta")
-	}
+	main2()
 }
-
-// var conn *pgxpool.Pool
-
-// func loadEnv() {
-// 	err := godotenv.Load()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
-// func createTable() {
-// 	query := `
-// 	CREATE TABLE IF NOT EXISTS posts (
-// 		id SERIAL PRIMARY KEY,
-// 		title TEXT NOT NULL,
-// 		content TEXT,
-// 		author TEXT NOT NULL
-// 	);
-// 	`
-
-// 	_, err := conn.Exec(context.Background(), query)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	fmt.Println("Table posts created")
-// }
-
-// func insertPostWithReturn() {
-// 	title := "Post 3"
-// 	content := "Conteúdo 3"
-// 	author := "Rudson Alves"
-// 	query := `
-// 	INSERT INTO posts (title, content, author)
-// 		VALUES ($1, $2, $3)
-// 		RETURNING id
-// 	`
-
-// 	row := conn.QueryRow(context.Background(), query, title, content, author)
-// 	var id int
-// 	err := row.Scan(&id)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	fmt.Printf("Post id: %d\n", id)
-// }
-
-// func selectById(id int) {
-// 	query := `
-// 	SELECT title, content, author FROM posts WHERE id = $1
-// 	`
-
-// 	row := conn.QueryRow(context.Background(), query, id)
-// 	var title, content, author string
-
-// 	err := row.Scan(&title, &content, &author)
-// 	// if err == pgxpool. .ErrNoRows {
-// 	// 	fmt.Printf("No post find from id: %d\n", id)
-// 	// 	return
-// 	// } else
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Printf("title: %s, content = %s, author = %s\n", title, content, author)
-// }
-
-// type Post struct {
-// 	Id      int
-// 	Title   string
-// 	Content string
-// 	Author  string
-// }
-
-// func (p Post) String() string {
-// 	return fmt.Sprintf("Post id: %d  title: '%s'  content: '%s'  author: '%s'",
-// 		p.Id,
-// 		p.Title,
-// 		p.Content,
-// 		p.Author,
-// 	)
-// }
-
-// func selectAllPosts() (posts []Post) {
-// 	query := `
-// 	SELECT id, title, content, author FROM posts
-// 	`
-
-// 	rows, err := conn.Query(context.Background(), query)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer rows.Close()
-
-// 	if err := rows.Err(); err != nil {
-// 		panic(err)
-// 	}
-
-// 	for rows.Next() {
-// 		var post Post
-// 		err := rows.Scan(&post.Id, &post.Title, &post.Content, &post.Author)
-// 		if err != nil {
-// 			panic(err)
-// 		}
-// 		posts = append(posts, post)
-// 	}
-
-// 	return
-// }
-
-// func main() {
-// 	loadEnv() // Loading .env variables
-// 	var err error
-
-// 	dbUrl := os.Getenv("DATABASE_URL")
-// 	fmt.Printf("Connection Url: %s\n", dbUrl)
-// 	conn, err = pgxpool.New(context.Background(), dbUrl)
-// 	if err != nil {
-// 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-// 		panic(err)
-// 	}
-// 	defer conn.Close()
-
-// 	createTable()
-// 	// insertPostWithReturn()
-// 	selectById(2)
-// 	posts := selectAllPosts()
-
-// 	for _, post := range posts {
-// 		fmt.Println(post)
-// 	}
-// }
-
-// func main() {
-// 	// h := slog.NewJSONHandler(os.Stdout, nil)
-// 	h := slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{
-// 		Level: slog.LevelDebug,
-// 		// AddSource: true,
-// 	})
-// 	log := slog.New(h)
-
-// 	log.Info("Info messages")
-// 	log.Debug("Debug message", "request_id", 1, "user", "Rudson")
-// 	log.Warn("Warn message")
-// 	log.Error("Error message", "request_id", 1)
-// }
-
-// func main() {
-// 	err := godotenv.Load()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	port := os.Getenv("SERVER_PORT")
-// 	host := os.Getenv("HOST")
-
-// 	fmt.Printf("Servidor está rodando na porta: %s:%s/", host, port)
-
-// }
-
-// func main() {
-// 	var port int
-// 	var host string
-// 	var verbose bool
-
-// 	flag.IntVar(&port, "port", 7000, "Server port")
-// 	flag.StringVar(&host, "host", "suzail.com", "Hostname")
-// 	flag.BoolVar(&verbose, "v", false, "Verbose mode")
-
-// 	flag.Parse()
-
-// 	if verbose {
-// 		fmt.Printf("Server is running in: %s:%d\n", host, port)
-// 	} else {
-// 		fmt.Printf("%s:%d\n", host, port)
-// 	}
-// }
-
-// type Config struct {
-// 	Server struct {
-// 		Port      int
-// 		Host      string
-// 		StaticDir string
-// 	}
-// }
-
-// func main() {
-// 	file, err := os.Open("config.json")
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	defer file.Close()
-
-// 	var config Config
-// 	err = json.NewDecoder(file).Decode(&config)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-
-// 	fmt.Printf("Static dir: %s\n", config.Server.StaticDir)
-// 	fmt.Printf("Host: %s:%d\n", config.Server.Host, config.Server.Port)
-// }
---
+++ internal/mailer/smtp.go
@@ -0,0 +1,42 @@
+package mailer
+
+import "gopkg.in/gomail.v2"
+
+type SMTPConfig struct {
+	From     string
+	Host     string
+	Port     int
+	UserName string
+	Password string
+}
+
+type smtpMailService struct {
+	from   string
+	dialer *gomail.Dialer
+}
+
+func NewSmtpMailService(cfg SMTPConfig) MailService {
+	dialer := gomail.NewDialer(cfg.Host, cfg.Port, cfg.UserName, cfg.Password)
+	return &smtpMailService{
+		from:   cfg.From,
+		dialer: dialer,
+	}
+}
+
+// Send implements MailService.
+func (ss *smtpMailService) Send(msg MailMessage) error {
+	m := gomail.NewMessage()
+	m.SetHeader("From", ss.from)
+	m.SetHeader("To", msg.To...)
+	// m.SetAddressHeader("Cc", "dan@example.com", "Dan")
+	m.SetHeader("Subject", msg.Subject)
+	if msg.IsHtml {
+		m.SetBody("text/html", string(msg.Body))
+	} else {
+		m.SetBody("text/plain", string(msg.Body))
+	}
+	// m.Attach("/home/Alex/lolcat.jpg")
+
+	// Send the email to Bob, Cora and Dan.
+	return ss.dialer.DialAndSend(m)
+}
